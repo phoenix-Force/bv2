09fa8f31c17074e716398ccaf37acd90df87000c
phoenix-Force <paradox.joe.007@gmail.com>
2020-05-18 19:35:28 +0530

    initial commit

---
+++ layouts/user.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <div>
+        <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-export</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </div>
+    <div style="background: url('https://images.unsplash.com/photo-1549740425-5e9ed4d8cd34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');background-repeat: no-repeat;background-size: cover;">
+        <v-hover open-delay="200">
+            <v-container :fluid=true>
+                <v-row align="center">
+                    <v-col cols="12" sm="12" md="12">
+                        <v-responsive class="overflow-y-auto" max-height="400">
+                          <nuxt />
+                        </v-responsive>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-hover>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+            hover: true,
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+<v-responsive class="overflow-y-auto" max-height="400">
+    <div class="pa-6 text-center">
+        Scroll down
+    </div>
+
+    <v-responsive height="200vh" class="text-center pa-2">
+        <v-responsive min-height="50vh"></v-responsive>
+        <div class="text-center body-2 mb-12">The card will appear below:</div>
+
+        <v-lazy v-model="isActive" :options="{
+          threshold: .5
+        }" min-height="200" transition="fade-transition">
+            <v-card class="mx-auto" max-width="336">
+                <v-card-title>Card title</v-card-title>
+
+                <v-card-text>
+                    Phasellus magna. Quisque rutrum. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Aliquam lobortis. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna.
+
+                    In turpis. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In turpis. Pellentesque dapibus hendrerit tortor. Ut varius tincidunt libero.
+                </v-card-text>
+            </v-card>
+        </v-lazy>
+    </v-responsive>
+</v-responsive>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        isActive: false,
+    }),
+}
+</script>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+    layout: "user",
+    components: {
+        Logo,
+        VuetifyLogo
+    },
+    data() {
+        return {
+          isActive: false,
+            items: [{
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+                },
+            ],
+        }
+    },
+};
+</script>
